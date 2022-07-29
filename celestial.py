7a2db2c91efdd669c6969451d9f217993b43df13
hoverslam <hofmann.christopher@outlook.com>
2022-07-29 12:43:53 +0200

    Initial upload

---
+++ celestial.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+from vector import Vector3
+
+
+class Body:
+    """This class represents celestial bodies like stars, planets, asteroids, etc.
+    """
+    
+    def __init__(self, position: Vector3, velocity: Vector3, mass: float, radius: float,
+                 name: str = "", color: str = "lightgray") -> None:
+        """Initialize class.
+
+        Args:
+            position (Vector3): The position from the origin in km.
+            velocity (Vector3): The velocity of this body in km.
+            mass (float): The mass of this body in kg.
+            radius (float): The radius of this body in km.
+            name (str, optional): The name of this body. Defaults to "".
+            color (str, optional): The color in which the body is displayed on the canvas. Defaults to "lightgray".
+        """
+        self.position = position
+        self.velocity = velocity
+        self.mass = mass
+        self.radius = radius
+        self.name = name
+        self.color = color
+        self.force = Vector3()
+        
+    def update(self) -> None:
+        raise NotImplementedError
+    
+    def distance_to(self, b: Body) -> float:
+        raise NotImplementedError
+    
+    def gravitational_force(self, b: Body) -> Vector3:
+        raise NotImplementedError
+    
+    def add_force(self, b: Body) -> None:
+        raise NotImplementedError
+    
+    def reset_force(self, b: Body) -> None:
+        raise NotImplementedError
