55f198b0508151b169b40d2b2891baeae1f43e86
hoverslam <hofmann.christopher@outlook.com>
2022-08-02 15:32:58 +0200

    Add Ceres, Pallas, and Vesta

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from celestial import Body
 
 
 if __name__ == "__main__":
-     # The sun and inner planets of our solar system
+     # Add bodies
      bodies = [Body(Vector3(), Vector3(),
                     1.98847e30, 6.957e8, color="yellow"),
                Body(Vector3(-2.565786669256473e10, 1.459776345868732e11, -1.548952074754238e7),
@@ -18,7 +18,16 @@ if __name__ == "__main__":
                     4.86732e24, 6.051e6, name="Venus", color="navajowhite"),
                Body(Vector3(-4.726172010176350e10, -4.671443491995941e10, 4.087355549094304e9),
                     Vector3(2.476489737911525e4, -3.197373466563876e4, -4.885743869363329e3),
-                    3.30104e23, 2.439e6, name="Mercury", color="gray")]
+                    3.30104e23, 2.439e6, name="Mercury", color="gray"),
+               Body(Vector3(-3.250173761375654e11, -2.203419016964185e11, 5.291759744142130e10),
+                    Vector3(9.165639992762751e3, -1.610304458359491e4, -2.199191038589312e3),
+                    9.38350e20, 4.697e5, name="Ceres"),
+               Body(Vector3(-3.453585004297552e11, 2.151464493938778e10, 1.484082838948810e10),
+                    Vector3(-3.944259837966826e3, -1.692743937506040e4, 1.202778577972903e4),
+                    2.11000e20, 2.700e5, name="Pallas"),
+               Body(Vector3(2.896729116971705e11, -1.781911555651397e11, -2.990483159764896e10),
+                    Vector3(1.170943386515238e4, 1.610135702679393e4, -1.907600473100395e3),
+                    2.59000e20, 2.627e5, name="Vesta")]
 
      # Run simulation
      sim = BruteForce(bodies, 2, True)
