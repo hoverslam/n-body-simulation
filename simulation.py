339b26e3b4c85a6d13ccfed9af7fedf8964169f8
hoverslam <hofmann.christopher@outlook.com>
2022-08-03 10:38:05 +0200

    Add body merge, change display size of bodies

--- simulation.py
+++ simulation.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from celestial import Body
 from vector import Vector3
-
+import math
 
 class Simulation:
     """This is the parent class for the different simulations. The specifc calculation is implemented in the subclass.
@@ -48,7 +48,7 @@ class Simulation:
         self.set_limits()
         self.remove_ticks()
         for b in self.bodies:
-            marker_size = b.radius / 6.957e8 * 1e2 / self.size    # make the marker a seeable size
+            marker_size = math.sqrt(b.radius / 6.957e8) * 2    # make the marker a seeable size
             self.ax.scatter(b.position[0], b.position[1], b.position[2], s=marker_size, c=b.color)
             if self.show_names and b.name != "":
                 self.ax.text(b.position[0], b.position[1], b.position[2], b.name, color=b.color, fontsize=4)
@@ -72,10 +72,37 @@ class Simulation:
     def set_limits(self) -> None:
         """Set the limit of the axis given by the size parameter.
         """
-        limit = self.size * 149_597_870_700    # convert AU in meter
+        limit = self.size * 149_597_870_700    # convert AU to meter
         self.ax.set_xlim((-limit, limit))
         self.ax.set_ylim((-limit, limit))
         self.ax.set_zlim((-limit, limit))
+        
+    def merge_bodies(self, b1: Body, b2: Body) -> None:
+        """Merge to bodies into a new one. This works better if the leap-frog timestep dt is 
+        relatively small (e.g. 10000).  
+
+        Args:
+            b1 (Body): A celestial object.
+            b2 (Body): A celestial object.
+        """
+        if b2.mass > b1.mass:
+            b1, b2 = b2, b1    # Delete smaller and update bigger body
+        b1.mass += b2.mass
+        b1.velocity += b2.velocity
+        b1.radius += math.sqrt(b2.radius)    # A very crude/unscientific new radius
+        self.remove_body(b2)
+        
+    def remove_body(self, b: Body) -> None:
+        """Remove a body from the list.
+
+        Args:
+            b (Body): A celestial object.
+        """
+        new_list = []        
+        for body in self.bodies:
+            if body is not b:
+                new_list.append(body)
+        self.bodies = new_list
                 
         
 class BruteForce(Simulation):
@@ -100,8 +127,13 @@ class BruteForce(Simulation):
         for i, first in enumerate(self.bodies):
             forces.append(Vector3())
             for second in self.bodies:
-                if first != second:
-                    forces[i] += first.gravitational_force(second)      
-        
+                if first is not second:
+                    # Merge bodies if distance is smaller than the sum of their radii
+                    if first.distance_to(second) < first.radius + second.radius:                        
+                        self.merge_bodies(first, second)
+                        return    # stop this iteration of force updates since a new body was created
+                    # Add force to the first body
+                    forces[i] += first.gravitational_force(second)   
+  
         for i, b in enumerate(self.bodies):        
-            b.move(forces[i], 25000)    # 25000 is a good trade off between accuracy and speed  
+            b.move(forces[i], 5000)    # 20000 is a good tradeoff between accuracy and speed
